bda5d229ea49660451cf2e7acdb95432f0640f81
Blake Martin <56556280+bamartin1618@users.noreply.github.com>
2022-02-16 21:06:47 -0600

    Update HashTableAssignment.py

--- HashTableAssignment.py
+++ HashTableAssignment.py
@@ -6,48 +6,53 @@
 
 import time
 import pandas
+import random
+from pandas_datareader.nasdaq_trader import get_nasdaq_symbols
 
-class HashTable:
+class HashTable: # Class to implement hash tables.
     
     def __init__(self, load_factor, resize_factor, buckets):
         
-        self.load_factor = load_factor
-        self.resize_factor = resize_factor
+        self.load_factor = load_factor # The load factor is the ratio of the number of keys to the number of buckets. It determines when we resize the hash table.
+        
+        self.resize_factor = resize_factor # The resize factor determines how much more space is allocated to the hash table when resizing 
+        # (e.g. if the resize factor is two, after resizing, a lookup table with 50 buckets will have 100 buckets.)
+        
         self.buckets = buckets
         
-        self.lookup_table = [[] for i in range(self.buckets)]
+        self.lookup_table = [[] for i in range(self.buckets)] # Initializes the hash table.
         
-        self.n = 0
+        self.n = 0 # Initializes the number of keys in the hash table.
         
-    def __setitem__(self, key, value):
+    def __setitem__(self, key, value): # Adds a key-value pair to the hash table.
         
-        hashed_key = hash(key) % self.buckets
+        hashed_key = hash(key) % self.buckets # Computes the hash of the key.
         
         found = False
         
         for index, element in enumerate(self.lookup_table[hashed_key]):
-            
+            # Iterate through all key-value pairs in the bucket corresponding to the hash. If our key is already present, we update the key's value.
             if element[0] == key:
                 self.lookup_table[hashed_key][index] = (key, value)
                 
                 found = True
                 break
                     
-        else:
+        else: # The key is not present in the bucket.
             self.lookup_table[hashed_key].append((key, value))
         
-        self.n += 1
+        self.n += 1 # Increment the number of keys.
         
 
-        if (self.n / self.buckets) >= self.load_factor:
+        if (self.n / self.buckets) >= self.load_factor: # Check to see if the hash table needs more space. If so, invoke the resize method on the hash table.
             self.resize(self.resize_factor)
 
-    def __getitem__(self, key):
+    def __getitem__(self, key): # Gets the value corresponding to a key from the hash table.
         
-        hashed_key = hash(key) % self.buckets
+        hashed_key = hash(key) % self.buckets # Computes the hash of the key.
 
         
-        for element in self.lookup_table[hashed_key]:
+        for element in self.lookup_table[hashed_key]: # Perform a search for the key in the bucket corresponding to the hash.
             
             if element[0] == key:
                 
@@ -55,23 +60,25 @@ class HashTable:
             
         return -1
     
-    def delete(self, key):
+    def delete(self, key): # Delete a key from the hash table.
         
         hashed_key = hash(key) % self.buckets
         
-        for index, element in enumerate(self.lookup_table[hashed_key]):
+        for index, element in enumerate(self.lookup_table[hashed_key]): # Perform a search for the key in the bucket corresponding to the hash.
             
             if element[0] == key:
                 
-                del self.lookup_table[hashed_key][index]
+                del self.lookup_table[hashed_key][index] # If found, delete the key.
+                break
 
                 
-    def resize(self, factor):
+    def resize(self, factor): # Resizes the hash table to allocate more space.
         
         resized_size = int(factor * self.buckets)
         
         resized_lookup_table = [[] for i in range(resized_size)]
         
+        # Move all key-value pairs from the present lookup table to the resized lookup table.
         for bucket in self.lookup_table:
 
             for key, value in bucket:
@@ -80,28 +87,25 @@ class HashTable:
         
                 resized_lookup_table[hashed_key].append([key, value])
             
-        self.lookup_table = resized_lookup_table
-        self.buckets = resized_size
+        self.lookup_table = resized_lookup_table # Update the lookup table to the resized lookup table.
+        self.buckets = resized_size # Update the number of buckets.
 
 
 # In[2]:
 
+nasdaq_data = get_nasdaq_symbols() # Retrieves a DataFrame of NASDAQ data.
 
-from pandas_datareader.nasdaq_trader import get_nasdaq_symbols
+table_data = nasdaq_data[['NASDAQ Symbol', 'Security Name']] # Extract NASDAQ symbols and their corresponding names.
 
-nasdaq_data = get_nasdaq_symbols()
-
-table_data = nasdaq_data[['NASDAQ Symbol', 'Security Name']]
-
-nasdaq_hash_table = HashTable(3, 2, 1000)
+nasdaq_hash_table = HashTable(3, 2, 1000) # Initialize a hash table and an empty array.
 nasdaq_array = []
 
-for symbol, name in table_data.to_numpy():
+for symbol, name in table_data.to_numpy(): # Fill out the hash table and the array.
 
     nasdaq_hash_table[symbol] = name
     nasdaq_array.append([symbol, name])
     
-def linear_search(array, symbol):
+def linear_search(array, symbol): # Searches for a symbol in an array
     
     for i, element in enumerate(array):
         if element[0] == symbol:
@@ -111,35 +115,33 @@ def linear_search(array, symbol):
 
 # In[4]:
 
-
-import random
-
-def main():
+def main(): # Program to compare the lookup speed of a hash table compared to an array.
     
     hash_table_time = 0
     array_time = 0
     
     
     for i in range(10000):
-        random_symbol = random.choice(nasdaq_data['NASDAQ Symbol'].to_numpy())
+        random_symbol = random.choice(nasdaq_data['NASDAQ Symbol'].to_numpy()) # Generate a random NASDAQ symbol.
 
         t0 = time.time()
         
-        nasdaq_hash_table[random_symbol]
+        nasdaq_hash_table[random_symbol] # Invoke the hash table's get method on the random symbol.
         
         t1 = time.time()
         
-        linear_search(nasdaq_array, random_symbol)
+        linear_search(nasdaq_array, random_symbol) # Invoke a linear search on the array to lookup the random symbol.
         t2 = time.time()
         
-        hash_table_time += t1-t0
+        hash_table_time += t1-t0 # Add the time it took to perform both tasks.
         array_time += t2-t1
         
     print(f"Hash Table Approach: {round(hash_table_time, 3)} seconds")
     print(f"Array Approach: {round(array_time, 3)} seconds")
     
 
-main()
+if __name__ == '__main__':
+    main()
 
 
 # In[ ]:
